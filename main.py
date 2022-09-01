232a5231c22d260cd742105c89dbd87a83d30be7
Alex Pavlenko <oleksa.pavlenko@gmail.com>
2022-09-01 20:01:35 +0300

    feat: filenames, collect everything into 2 files

--- main.py
+++ main.py
@@ -25,51 +25,58 @@ counters = {
 cfg = configparser.ConfigParser()
 cfg.read(".input.ini", encoding="utf-8")
 
-names = cfg.sections()
-for name in names:
-    media = cfg[name]
-    category = Category(media["category"])
-    counters[category] += 1
-    id = str(counters[category]).zfill(3)
+with open(f".out/media.lua", encoding="utf-8", mode="w") as definition:
+    with open(f".out/media.txt", encoding="utf-8", mode="w") as text:
+        names = cfg.sections()
 
-    definition_lines = ""
-    text_lines = ""
-    line_num = 0
-    for line in media["lines"].split("\n"):
-        if not line:
-            continue
-        line_num += 1
+        definition.write("RecMedia = RecMedia or {}\n\n")
 
-        definition_lines += f'    {{ text = "RM_UAMEDIA_{category.name}{id}_{line_num}", r = 0.0, g = 0.6, b = 0.4, codes = "BOR-1" }},\n'
+        for name in names:
+            media = cfg[name]
+            category = Category(media["category"])
+            counters[category] += 1
+            id = str(counters[category]).zfill(3)
 
-        text_line_decor = "[img=music]" if category == Category.CD else ""
-        text_lines += f'RM_UAMEDIA_{category.name}{id}_{line_num} = "{text_line_decor}{line}{text_line_decor}"\n'
+            definition_lines = ""
+            text_lines = ""
+            line_num = 0
+            # Collect definition and text lines.
+            for line in media["lines"].split("\n"):
+                if not line:
+                    continue
+                line_num += 1
 
-    with open(f".out/{name}_definition.txt", encoding="utf-8", mode="w") as f:
-        f.write(
-            f"""
-  RecMedia["RM_UAMEDIA_{category.name}{id}"] = {{
-  itemDisplayName = "RM_UAMEDIA_{category.name}{id}_name",
-  title = "RM_UAMEDIA_{category.name}{id}_title",
-  subtitle = nil,
-  author = "RM_UAMEDIA_{category.name}{id}_author",
-  extra = "RM_UAMEDIA_{category.name}{id}_extra",
-  spawning = 0,
-  category = "{category.value}",
-  lines = {{
+                definition_lines += f'    {{ text = "RM_UAMEDIA_{category.name}{id}_{line_num}", r = 0.0, g = 0.6, b = 0.4, codes = "BOR-1" }},\n'
+
+                text_line_decor = "[img=music]" if category == Category.CD else ""
+                text_lines += f'RM_UAMEDIA_{category.name}{id}_{line_num} = "{text_line_decor}{line}{text_line_decor}"\n'
+
+            # Write definition.
+            definition.write(
+                f"""
+RecMedia["RM_UAMEDIA_{category.name}{id}"] = {{
+itemDisplayName = "RM_UAMEDIA_{category.name}{id}_name",
+title = "RM_UAMEDIA_{category.name}{id}_title",
+subtitle = nil,
+author = "RM_UAMEDIA_{category.name}{id}_author",
+extra = "RM_UAMEDIA_{category.name}{id}_extra",
+spawning = 0,
+category = "{category.value}",
+lines = {{
 {definition_lines}  }},
 }};
-  """.strip()
-        )
+    """.strip()
+                + "\n\n"
+            )
 
-    with open(f".out/{name}_text.txt", encoding="cp1251", mode="w") as f:
-        f.write(
-            f"""
+            # Write text.
+            text.write(
+                f"""
 RM_UAMEDIA_{category.name}{id}_name = "{media["name"]}"
 RM_UAMEDIA_{category.name}{id}_title = "{media["title"]}"
 RM_UAMEDIA_{category.name}{id}_author = "{media["author"]}"
 RM_UAMEDIA_{category.name}{id}_extra = "{media["extra"]}"
-
 {text_lines}
-  """.strip()
-        )
+    """.strip()
+                + "\n\n"
+            )
